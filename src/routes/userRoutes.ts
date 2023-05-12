395228678902b71d09923f5cff6fea0a449372b5
M.T. Oyelowo <oyelowomayowa@gmail.com>
2023-05-12 18:40:24 +0100

    Tweet CRUD Operations

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -34,6 +34,10 @@ router.get("/", async (req, res) => {
 router.get("/:id", async (req, res) => {
     const { id } = req.params;
     const user = await prisma.user.findUnique({ where: { id: Number(id) } })
+
+    if (!user) {
+        return res.status(404).json({ error: "User not found!" })
+    }
     res.json(user)
 });
 
