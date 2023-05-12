89e76008aab9f9b0432e72d06ab99dc7fe2fe828
M.T. Oyelowo <oyelowomayowa@gmail.com>
2023-05-12 19:36:00 +0100

    Prisma query relationships for tweets and users

--- src/routes/tweetRoutes.ts
+++ src/routes/tweetRoutes.ts
@@ -25,14 +25,14 @@ router.post('/', async (req, res) => {
 
 //List Tweets
 router.get("/", async (req, res) => {
-    const allTweets = await prisma.tweet.findMany();
+    const allTweets = await prisma.tweet.findMany({ include: { user: { select: { id: true, name: true, username: true, image: true } } } });
     res.json(allTweets)
 });
 
 //Get One Tweet
 router.get("/:id", async (req, res) => {
     const { id } = req.params;
-    const tweet = await prisma.tweet.findUnique({ where: { id: Number(id) } })
+    const tweet = await prisma.tweet.findUnique({ where: { id: Number(id) }, include: { user: true } })
 
     if (!tweet) {
         return res.status(404).json({ error: "Tweet not found!" })
--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -26,14 +26,16 @@ router.post('/', async (req, res) => {
 
 //List Users
 router.get("/", async (req, res) => {
-    const allUser = await prisma.user.findMany()
+    const allUser = await prisma.user.findMany({
+        //select: { id: true, name: true, image: true }
+    })
     res.json(allUser)
 });
 
 //Get One User
 router.get("/:id", async (req, res) => {
     const { id } = req.params;
-    const user = await prisma.user.findUnique({ where: { id: Number(id) } })
+    const user = await prisma.user.findUnique({ where: { id: Number(id) }, include: { tweets: true } })
 
     if (!user) {
         return res.status(404).json({ error: "User not found!" })
